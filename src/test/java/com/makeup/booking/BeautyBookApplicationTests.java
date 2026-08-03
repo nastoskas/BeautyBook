2f0fcbbecc17fc110cec2757ffa32ff55fed4405
SimonaN <nastoskasimona40@gmail.com>
2026-08-03 15:17:37 +0200

    Initial Spring Boot project setup

---
+++ src/test/java/com/makeup/booking/BeautyBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makeup.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautyBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
