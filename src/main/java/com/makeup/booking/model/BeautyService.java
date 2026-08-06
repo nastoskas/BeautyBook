956791135f2b2f082c884e9183b0cf603043331a
SimonaN <nastoskasimona40@gmail.com>
2026-08-06 21:18:16 +0200

    Created BeautyService entity

---
+++ src/main/java/com/makeup/booking/model/BeautyService.java
@@ -0,0 +1,37 @@
+package com.makeup.booking.model;
+
+import com.makeup.booking.common.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "beauty_services")
+public class BeautyService extends BaseEntity {
+    @Column(nullable = false, unique = true)
+    private String name;
+    private String imagePath;
+    @NotBlank
+    @Column(length = 1000)
+    private String description;
+    @Positive(message = "Duration must be a greater than zero")
+    private int duration; // in minutes
+    @NotNull
+    @Positive(message = "Price must be a positive value")
+    private BigDecimal price;
+    @Column(nullable = false)
+    private boolean active = true;
+}
